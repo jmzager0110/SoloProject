d78dd6d99162497bcd1ee4c7597cad775e77a189
Jessica Zager <jmzager@gmail.com>
2021-12-30 20:21:38 -0600

    Added classes & controllers per techjobs-authentication for Authentication

---
+++ demo/src/main/java/com/example/demo/WebApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public  AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor((HandlerInterceptor) authenticationFilter());
+    }
+
+
+}
